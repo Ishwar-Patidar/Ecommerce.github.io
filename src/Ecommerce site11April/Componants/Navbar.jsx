3086524adae7faa489a9702350735c7b789d45ad
Manjeetmj <>
2023-04-29 16:58:01 +0530

    add routing link

--- src/Ecommerce site11April/Componants/Navbar.jsx
+++ src/Ecommerce site11April/Componants/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import proContext from "./EcommerseContext/ApIContext/ProductContext";
 import './Navbar.css'
 import PersonDetail from "./PersonDetail";
@@ -53,10 +54,11 @@ function Navbar() {
                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className=" bi bi-cart text-dark" viewBox="0 0 16 16">
                                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                                             </svg>
-                                            <span className="position-absolute top-2 start-80 translate-middle badge rounded-pill bg-primary" style={{ fontSize: "11px" }}>
-                                                {cartProductCount}
-                                                <span className="visually-hidden">unread messages</span>
+                                        
+                                            <span>
+                                            <Link className="nav-link position-absolute top-2 start-80 translate-middle badge rounded-pill bg-primary" to="/store">{cartProductCount ? cartProductCount : ''}</Link>
                                             </span>
+                                                <span className="visually-hidden">unread messages</span>
                                         </a>
                                     </li>
 
